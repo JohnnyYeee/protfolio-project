d2d712049e52e0365ca04e16753c179e19a82ca3
Huasheng Ye <johnnyye9625@gmail.com>
2021-10-18 05:34:12 -0700

    edit job.html

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = RichTextField(blank=True,null=True)
+    #summary = models.CharField(max_length=500)
