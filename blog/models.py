61b94b13784d0221c1a7078fadac3783a79a9c94
Huasheng Ye <johnnyye9625@gmail.com>
2021-10-17 21:57:05 -0700

    change the image setting

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
-    body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    body = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='images/',blank=True,null=True)
 
     def __str__(self):
         return self.title
